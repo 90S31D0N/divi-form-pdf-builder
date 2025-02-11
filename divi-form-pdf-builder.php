885fa5bb18d04f80e68c3ce14a1ef521cd276d59
P0S31D0N <noelschaller32@outlook.com>
2025-02-11 23:28:36 +0100

    add auto updater

--- divi-form-pdf-builder.php
+++ divi-form-pdf-builder.php
@@ -7,6 +7,7 @@ Version: 1.0.0
 Author:  Noël Schaller
 Author URI: https://deine-website.com
 License: GPL2
+GitHub Plugin URI: 90S31D0N/divi-from-pdf-builder
 */
 
 if (!defined('ABSPATH')) {
@@ -22,3 +23,6 @@ function my_custom_plugin_deactivate() {
     // Deaktivierungscode hier
 }
 register_deactivation_hook(__FILE__, 'my_custom_plugin_deactivate');
+
+//GitHub Updater einbinden
+require_once plugin_dir_path(__FILE__) . 'git-updater/git-updater.php';
